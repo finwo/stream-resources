c2e6fc3f14181b63a033a5e82ef2e59a475260b6
Robin Bron <robin@finwo.nl>
2023-01-15 16:38:48 +0100

    Performance boost, no more array instantiation during polygon detection

--- src/stack/frame/polygon-detection.ts
+++ src/stack/frame/polygon-detection.ts
@@ -4,22 +4,15 @@ export function insidePolygon(polygon, x, y) {
   for(const path of polygon) {
     // Iterate over lines in the path
     // const path = polygon[p];
-    for(let i=0; i<path.length; i++) {
+    for(let i=0; i < path.length; i++) {
 
       // Fetch path's points, relative to our position
-      const
-        pointA = [
-          path[i][0] - x,
-          path[i][1] - y,
-        ],
-        pointB = [
-          path[(i + 1) % path.length][0] - x,
-          path[(i + 1) % path.length][1] - y,
-        ];
+      const pointA = path[i],
+            pointB = path[(i + 1) % path.length];
 
-      if ((pointA[0] < 0) && (pointB[0] < 0)) continue; // Both points are to the left
-      if ((pointA[1] < 0) && (pointB[1] < 0)) continue; // Both points are below test
-      if ((pointA[1] > 0) && (pointB[1] > 0)) continue; // Both points are above test
+      if ((pointA[0] < x) && (pointB[0] < x)) continue; // Both points are to the left
+      if ((pointA[1] < y) && (pointB[1] < y)) continue; // Both points are below test
+      if ((pointA[1] > y) && (pointB[1] > y)) continue; // Both points are above test
 
       // Horizontal line = NOT crossing
       if (pointA[1] == pointB[1]) {
@@ -27,7 +20,7 @@ export function insidePolygon(polygon, x, y) {
       }
 
       // Crosses somewhere, don't care where
-      if ((pointA[0] >= 0) && (pointB[0] >= 0)) {
+      if ((pointA[0] >= x) && (pointB[0] >= x)) {
         crossings++;
         continue;
       }
@@ -42,13 +35,12 @@ export function insidePolygon(polygon, x, y) {
       const a = (right[1] - left[1]) / (right[0] - left[0]); // Slope
       const c = -((a * left[0]) - left[1]);                  // Constant
 
-      // // Plug in y=0 to get where it'll cross the X axis
+      // Plug in y to get where it'll cross the X axis
       // x = (y - c) / a
-      // x = (  - c) / a
-      const T = (-c) / a;
+      const T = (y-c) / a;
 
-      // Count as crossing if @ 0 or to the right
-      if (T >= 0) {
+      // Count as crossing if at x or to it's right
+      if (T >= x) {
         crossings++;
         continue;
       }
